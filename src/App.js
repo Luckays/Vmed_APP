57f09313e9c55d98ca26623f5a0d21351edf8aed
luckays <lukasbeloch@seznam.cz>
2020-02-24 17:14:24 +0000

    graph v1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect} from 'react';
 import './styles/main.scss'
 import api from "./api";
 import image from './logo.png'
-import calendar from "./calendar"
+import Calendar from "./calendar"
+import {Line} from 'react-chartjs-2'
+
+
 function App() {
 
     const [ selectedTable, setSelectedTable ] = useState('--');
@@ -10,6 +13,11 @@ function App() {
     const [ selectedColumn, setSelectedColumn] = useState('--');
     const [ columns, setColumns] = useState([]);
     const [ groupType, setGroupType ] = useState('--');
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+
+    const [chartData, setChartData] = useState([]);
+    const [chartLabels, setChartLabels] = useState([]);
     useEffect(() => {
         api.get('/tables')
             .then(res => {
@@ -19,11 +27,37 @@ function App() {
     }, []);
 
     useEffect(() => {
-        if (selectedTable === null) return;
+        if (selectedTable === '--') return;
         api.post('/columns', { tableName: selectedTable })
             .then(res => setColumns(res.data.data))
     }, [selectedTable]);
 
+
+  /*  useEffect(() => {
+        if (groupType === '--') return;
+        api.post('/fce', { group_type: groupType})
+            .then(res => setGroupType(res.data.data))
+
+    }, [groupType]);// input pouze pri zmene defi hodnoty, bez pri jakoliv zmene, prazdne jen pri prvni*/
+
+    useEffect(() => {
+        console.log(selectedTable,selectedColumn,startDate,endDate,groupType)
+        if (startDate === null || endDate === null || groupType === '--' ||groupType === undefined|| selectedTable === '--'||selectedColumn === '--') return;
+        api.post('/fce', {
+            table_name: selectedTable,
+            column: selectedColumn,
+            from_date:startDate.format('YYYY-MM-DD HH:mm:ss'),
+            to_date:endDate.format('YYYY-MM-DD HH:mm:ss'),
+            group_type: groupType
+        })
+            .then(response => {
+                const values = response.data.map(i => i.sel_value)
+                setChartData(values)
+                const dates = response.data.map(i => i.date_day)
+                setChartLabels(dates)
+            })
+
+    },[selectedTable,selectedColumn,startDate,endDate,groupType])
 
   const onTableNameChange = e => setSelectedTable(e.target.value);
   const onColumnNameChange = e => setSelectedColumn(e.target.value);;
@@ -35,7 +69,13 @@ function App() {
   }, [selectedTable]);
 
    var  downloadTxtFile = () => {
-
+       api.post('/download', {
+           table_name: selectedTable,
+           column: selectedColumn,
+           from_date:startDate.format('YYYY-MM-DD HH:mm:ss'),
+           to_date:endDate.format('YYYY-MM-DD HH:mm:ss'),
+           group_type: groupType
+       })
     }
 
 
@@ -67,13 +107,16 @@ function App() {
             </div>
             <div className="col-4">
                 <label>Kalendar</label>
-              <calendar
+              <Calendar onChange={(startDate,endDate) => {
+                setStartDate(startDate);
+                    setEndDate(endDate);
+              }}
               />
             </div>
 
             <div className="col-2">
                 <label>Statistika</label>
-                <select onChange={onGroupTypeChange}>
+                <select onChange={onGroupTypeChange} value={groupType}>
                     <option>--</option>
                     <option>Součet</option>
                     <option>Maximum</option>
@@ -91,19 +134,22 @@ function App() {
         </div>
       </div>
 
-        <div className="container-vypis">
-
-        <p>Tabulka {selectedTable}</p>
-            <p>Sloupec {selectedColumn}</p>
-        <p> Typ {groupType}</p>
-
-    </div>
-
       <div className="container-graph">
-graph
+<Line
+    data={{
+        labels: chartLabels,
+        datasets: [{
+            label: selectedColumn,
+            backgroundColor: 'red',
+            borderColor: 'red',
+            data: chartData,
+            fill: false,
+        }]
+    }}
+/>
       </div>
     </div>
   );
 }
 
-export default calendar;
+export default App;
--- src/calendar.js
+++ src/calendar.js
@@ -4,34 +4,27 @@ import 'react-dates/initialize'
 import 'react-dates/lib/css/_datepicker.css'
 import {DateRangePicker} from "react-dates";
 
+const Calendar = ({onChange}) => {
+    const [focusedInput, setFocusedInput] = useState(false);
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
 
-class calendar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            startDate: null,
-            endDate: null
-        }
-    }
+    return (
+        <DateRangePicker
+            startDate={startDate} // momentPropTypes.momentObj or null,
+            startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+            endDate={endDate} // momentPropTypes.momentObj or null,
+            endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+            isOutsideRange={() => false}
+            onDatesChange={({startDate, endDate}) => {
+                setStartDate(startDate);
+                setEndDate(endDate)
+                onChange(startDate,endDate)
+            }} // PropTypes.func.isRequired,
+            focusedInput={focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+            onFocusChange={setFocusedInput} // PropTypes.func.isRequired,
+        />
+    )
+};
 
-    render() {
-        return (
-
-                <DateRangePicker
-                    startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-                    startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
-                    endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-                    endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-                    onDatesChange={({startDate, endDate}) => this.setState({
-                        startDate,
-                        endDate
-                    })} // PropTypes.func.isRequired,
-                    focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-                    onFocusChange={focusedInput => this.setState({focusedInput})} // PropTypes.func.isRequired,
-                />
-
-
-        )
-    }
-}
-export default calendar
+export default Calendar

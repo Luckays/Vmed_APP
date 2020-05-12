62c5d36806b3f4c9c18c9436904569ff094fafc6
Luckays <56603748+Luckays@users.noreply.github.com>
2020-05-12 13:06:07 +0200

    Update Head_title.js

--- src/components/Head_title.js
+++ src/components/Head_title.js
@@ -1,11 +1,11 @@
-import image from "./logo.png";
+//import image from "./logo.png"; <img src={image} alt="Logo" height="95" width="95" align="top"/>
 import React from "react"
 const HeadTitle =()=> {
 return(
     <div className="container-preface">
         <div className="text-white">
             <div className="text-center"><h1> Vizualizace meteorologických a environmentálních dat Geodetické observatoře
-                Pecný <img src={image} alt="Logo" height="95" width="95" align="top"/></h1></div>
+                Pecný </h1></div>
         </div>
     </div>
 )

adbe79324d460d129ade7fc3c7842d272ca84b64
luckays <lukasbeloch@seznam.cz>
2020-07-07 20:31:51 +0100

    style

--- src/pages/long_data_view.js
+++ src/pages/long_data_view.js
@@ -463,8 +463,8 @@ function Long_data_view() {
                         }}
 
 
-                        width='2150'
-                        height='600'
+                        width='2150' redraw
+                        height='600' redraw
                         options={{ maintainAspectRatio:false,responsive:true}}
 
                     />

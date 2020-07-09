771b1e00f74748ce65086aefb5da108df5f80a9e
luckays <lukasbeloch@seznam.cz>
2020-07-09 12:57:06 +0100

    tuning

--- src/pages/long_data_view.js
+++ src/pages/long_data_view.js
@@ -95,9 +95,9 @@ function Long_data_view() {
         console.log(selectedTable,selectedColumn,startDate,endDate,groupType)
         if (startDate === null || endDate === null || groupType === '--' ||groupType === undefined|| selectedTable === '--'||selectedColumn === '--') return;
         if (groupType2 === '--' ||groupType2 === undefined|| selectedTable2 === '--'||selectedColumn2 === '--') { setChartData2(null)
-            setColumns2(null)} ;
+            setColumns2('--')} ;
         if (groupType3 === '--' ||groupType3 === undefined|| selectedTable3 === '--'||selectedColumn3 === '--') { setChartData3(null)
-            setColumns3(null)} ;
+            setColumns3('--')} ;
         api.post(process.env.REACT_APP_DATA_ANALYSIS, {
             table_name: selectedTable,
             column: selectedColumn,
@@ -161,9 +161,9 @@ function Long_data_view() {
         console.log(selectedTable2,selectedColumn2,startDate,endDate,groupType2)
         if (startDate === null || endDate === null || groupType2 === '--' ||groupType2 === undefined|| selectedTable2 === '--'||selectedColumn2 === '--') return;
         if (groupType === '--' ||groupType === undefined|| selectedTable === '--'||selectedColumn === '--') { setChartData(null)
-            setColumns(null)} ;
+            setColumns('--')} ;
         if (groupType3 === '--' ||groupType3 === undefined|| selectedTable3 === '--'||selectedColumn3 === '--') { setChartData3(null)
-            setColumns3(null)} ;
+            setColumns3('--')} ;
         api.post(process.env.REACT_APP_DATA_ANALYSIS, {
             table_name: selectedTable2,
             column: selectedColumn2,
@@ -227,9 +227,9 @@ function Long_data_view() {
         console.log(selectedTable3,selectedColumn3,startDate,endDate,groupType3)
         if (startDate === null || endDate === null || groupType3 === '--' ||groupType3 === undefined|| selectedTable3 === '--'||selectedColumn3 === '--') return
         if (groupType === '--' ||groupType === undefined|| selectedTable === '--'||selectedColumn === '--') { setChartData(null)
-            setColumns(null)} ;
+            setColumns('--')} ;
         if (groupType2 === '--' ||groupType2 === undefined|| selectedTable2 === '--'||selectedColumn2 === '--') { setChartData2(null)
-            setColumns2(null)} ;
+            setColumns2('--')} ;
         api.post(process.env.REACT_APP_DATA_ANALYSIS, {
             table_name: selectedTable3,
             column: selectedColumn3,

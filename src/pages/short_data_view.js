eec5addab0f320dbf909ba3d2d45304ab8433368
luckays <lukasbeloch@seznam.cz>
2020-07-29 08:20:55 +0100

    V_3

--- src/pages/short_data_view.js
+++ src/pages/short_data_view.js
@@ -36,7 +36,7 @@ function Short_data_view() {
         api.post(process.env.REACT_APP_DATA_DAY, {
             table_name:process.env.REACT_APP_FIRST_TABLE,
             column:process.env.REACT_APP_FIRST_COLUMN,
-            date:Date.now()
+            date:moment(Date.now()).format('YYYY-MM-DD')
         })
             .then(response => {
                 const values = response.data.map(i => i.sel_value);
